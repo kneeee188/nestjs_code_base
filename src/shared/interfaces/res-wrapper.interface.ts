c222b935291e0ee166349466f817df8485e2e33f
heal <make.abundant@gmail.com>
2023-11-16 17:35:01 +0900

    init(res-wrapper): static method, interfaces

---
+++ src/shared/interfaces/res-wrapper.interface.ts
@@ -0,0 +1,20 @@
+import { IInfiniteScroll } from './infinite-scroll.interface';
+import { IPagination } from './pagination.interface';
+
+export interface IResWrapSingle<Type> {
+  data: Type;
+}
+
+export interface IResWrapList<Type> {
+  data: Type[];
+}
+
+export interface ResWrapPagination<Type> {
+  data: Type[];
+  pagination: IPagination;
+}
+
+export interface ResWrapInfiniteScroll<Type> {
+  data: Type[];
+  infiniteScroll: IInfiniteScroll;
+}
---
+++ src/shared/static-utils/response-wrapper.static.ts
@@ -0,0 +1,42 @@
+import { IInfiniteScroll } from '../interfaces/infinite-scroll.interface';
+import { IPagination } from '../interfaces/pagination.interface';
+import {
+  IResWrapList,
+  IResWrapSingle,
+  ResWrapInfiniteScroll,
+  ResWrapPagination,
+} from '../interfaces/res-wrapper.interface';
+
+export default class ResWrapper {
+  static single<T>(obj: T): IResWrapSingle<T> {
+    return {
+      data: obj,
+    };
+  }
+
+  static list<T>(obj: T[]): IResWrapList<T> {
+    return {
+      data: obj,
+    };
+  }
+
+  static listWithPagination<T>(
+    pagination: IPagination,
+    list: T[],
+  ): ResWrapPagination<T> {
+    return {
+      pagination,
+      data: list,
+    };
+  }
+
+  static listWithInfiniteScroll<T>(
+    infiniteScroll: IInfiniteScroll,
+    list: T[],
+  ): ResWrapInfiniteScroll<T> {
+    return {
+      infiniteScroll,
+      data: list,
+    };
+  }
+}
